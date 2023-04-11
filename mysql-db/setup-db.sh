3657c8f4550cce1d3577f0fda50fa1a5762ce1f8
bartwr <mail@bartroorda.nl>
2023-04-11 17:43:01 +0200

    Install @mui/material package

--- mysql-db/setup-db.sh
+++ mysql-db/setup-db.sh
@@ -2,7 +2,7 @@
 #source https://earthly.dev/blog/docker-mysql/
 
 CWD=$(pwd)
-source "$CWD/../.env"
+source "../.env"
 
 if [ ! -$PASSWD ]; then
     echo "Set the password for mysql server in the .env file"
@@ -11,26 +11,29 @@ fi
 
 read -p "Warning: Running this scripts will delete the existing database. Continue? (yes/no) " yn
 case $yn in 
-	yes ) ;;
-	no ) echo exiting...;
-		exit;;
-	* ) echo invalid response;
-		exit 1;;
+    yes ) ;;
+    no ) echo exiting...;
+        exit;;
+    * ) echo invalid response;
+        exit 1;;
 esac
 
 echo "remove existing database and docker container"
 docker stop veiligstallen-mysql
 docker rm veiligstallen-mysql
 
+echo enter your password to delete the database files
+sudo rm -rf $CWD/persist-db
+mkdir -p $CWD/persist-db
 
-printf $PASSWD > "$CWD/secrets/mysql-root-password"
+echo $PASSWD > "$CWD/secrets/mysql-root-password"
 
 echo "create veiligstallen mysql database"
 docker run --name veiligstallen-mysql -d \
-    -p 3308:3306 \
+    -p $PORT:3306 \
     -e MYSQL_ROOT_PASSWORD_FILE=run/secrets/mysql-root-password \
-    -v mysql:$CWD/persist-db \
-    -v ~/dev/fietsberaad-veiligstallen-app/mysql-db/secrets:/run/secrets \
+    -v $CWD/persist-db:/var/lib/mysql \
+    -v $CWD/secrets:/run/secrets \
     mysql:5.7
 # --restart unless-stopped \
 
