7c2ef76e6069261f8dbb5b272646878bebe2dbcc
bartwr <mail@bartroorda.nl>
2023-05-09 15:45:47 +0200

    Updated Card so it reuses ParkinFacilityBlock

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -11,14 +11,14 @@ interface Props {
 const CardList: React.FC<Props> = ({ cards, cardsPerSlide = 3 }) => {
   const [ref] = useKeenSlider<HTMLDivElement>({
     slides: {
-      perView: 3,
+      perView: typeof window !== 'undefined' ? (window.innerWidth / 315) : 1.3,// slides are 315px in width
       spacing: 15,
     },
   });
 
   return (
     <div className="card-list">
-      <div ref={ref} className="card-list__slides keen-slider">
+      <div ref={ref} className="card-list__slides keen-slider px-5">
         {cards.map((card, index) => (
           <Card key={"c-" + card.ID} {...card} />
         ))}
