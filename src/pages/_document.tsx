74fa8976c60f8170814f1b03d1c7ff6a81e124f9
bartwr <mail@bartroorda.nl>
2023-07-22 11:38:15 +0200

    Added icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,15 +10,15 @@ class MainDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="application-name" content="PWA App" />
+          <meta name="application-name" content="VeiligStallen" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-          <meta name="apple-mobile-web-app-title" content="PWA App" />
-          <meta name="description" content="Best PWA App in the world" />
+          <meta name="apple-mobile-web-app-title" content="VeiligStallen" />
+          <meta name="description" content="De kortste weg naar een veilige plek voor uw fiets" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-          <meta name="msapplication-TileColor" content="#2B5797" />
+          {/*<meta name="msapplication-config" content="/icons/browserconfig.xml" />*/}
+          <meta name="msapplication-TileColor" content="#15AEEF" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#000000" />
 
@@ -30,22 +30,22 @@ class MainDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+          {/*<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />*/}
           <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://yourdomain.com" />
-          <meta name="twitter:title" content="PWA App" />
-          <meta name="twitter:description" content="Best PWA App in the world" />
-          <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
-          <meta name="twitter:creator" content="@DavidWShadow" />
+          <meta name="twitter:url" content="https://veiligstallen.addbrainz.com" />
+          <meta name="twitter:title" content="VeiligStallen" />
+          <meta name="twitter:description" content="De kortste weg naar een veilige plek voor uw fiets" />
+          <meta name="twitter:image" content="https://veiligstallen.addbrainz.com/icons/android-chrome-192x192.png" />
+          <meta name="twitter:creator" content="@verkeer_vervoer" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="PWA App" />
-          <meta property="og:description" content="Best PWA App in the world" />
-          <meta property="og:site_name" content="PWA App" />
-          <meta property="og:url" content="https://yourdomain.com" />
-          <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
+          <meta property="og:title" content="VeiligStallen" />
+          <meta property="og:description" content="De kortste weg naar een veilige plek voor uw fiets" />
+          <meta property="og:site_name" content="VeiligStallen" />
+          <meta property="og:url" content="https://veiligstallen.addbrainz.com" />
+          <meta property="og:image" content="https://veiligstallen.addbrainz.com/icons/apple-touch-icon.png" />
 
           {/* <!-- apple splash screen images -->
           <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
