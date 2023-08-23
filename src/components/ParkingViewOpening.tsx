99792550e57363167a8ef933e2c94dfdaed50ae0
bartwr <mail@bartroorda.nl>
2023-08-23 21:34:46 +0200

    Don't show null values in ParkingViewOpening

--- src/components/ParkingViewOpening.tsx
+++ src/components/ParkingViewOpening.tsx
@@ -11,32 +11,35 @@ const ParkingViewOpening = ({ parkingdata }: { parkingdata: any }) => {
   ): React.ReactNode => {
     const wkday = new Date().getDay();
 
-    const tmpopen: Date = new Date(Date.parse(parkingdata["Open_" + day]));
+    let value = '?';
+    if(parkingdata["Open_" + day]) {
+      const tmpopen: Date = new Date(Date.parse(parkingdata["Open_" + day]));
+      // NB: times are stored as UTC times (no timezone offset)
+      const hoursopen = tmpopen.getUTCHours();
+      const minutesopen = String(tmpopen.getUTCMinutes()).padStart(2, "0");
+
+      const tmpclose: Date = new Date(Date.parse(parkingdata["Dicht_" + day]));
+      const hoursclose = tmpclose.getUTCHours();
+      const minutesclose = String(tmpclose.getUTCMinutes()).padStart(2, "0");
+
+      value = `${hoursopen}:${minutesopen} - ${hoursclose}:${minutesclose}`;
   
-    // NB: times are stored as UTC times (no timezone offset)
-    const hoursopen = tmpopen.getUTCHours();
-    const minutesopen = String(tmpopen.getUTCMinutes()).padStart(2, "0");
-
-    const tmpclose: Date = new Date(Date.parse(parkingdata["Dicht_" + day]));
-    const hoursclose = tmpclose.getUTCHours();
-    const minutesclose = String(tmpclose.getUTCMinutes()).padStart(2, "0");
-
-    let value = `${hoursopen}:${minutesopen} - ${hoursclose}:${minutesclose}`;
-
-
-    if(hoursopen===0 && minutesopen==='00' && hoursclose===0 && minutesclose==='00') {
-      value= 'gesloten' 
-    } else {
-      let diff = Math.abs((tmpclose.getTime() - tmpopen.getTime()) / 1000);
-      if(diff>=86340 || diff===0) {
-        value = '24h'
+      if(hoursopen===0 && minutesopen==='00' && hoursclose===0 && minutesclose==='00') {
+        value= 'gesloten' 
+      } else {
+        let diff = Math.abs((tmpclose.getTime() - tmpopen.getTime()) / 1000);
+        if(diff>=86340 || diff===0) {
+          value = '24h'
+        }
       }
     }
 
     return (
       <>
         <div className={wkday + 1 === dayidx ? "font-bold" : ""}>{label}</div>
-        <div className={"text-right" + (wkday + 1 === dayidx ? " font-bold" : "")}>{value}</div>
+        <div className={"text-right" + (wkday + 1 === dayidx ? " font-bold" : "")}>
+          {value}
+        </div>
       </>
     );
   };
