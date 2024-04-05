6e5beef9ae3a3c98931132b37f47960a134e2544
bartwr <mail@bartroorda.nl>
2024-04-05 13:29:07 +0200

    Add "Koop abonnement" knop to ParkingView modal

--- src/components/parking/ParkingViewAbonnementen.tsx
+++ src/components/parking/ParkingViewAbonnementen.tsx
@@ -1,25 +1,25 @@
-import React, {Fragment, useEffect, useState} from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 import HorizontalDivider from "~/components/HorizontalDivider";
 import { Button } from "~/components/Button";
 import SectionBlock from "~/components/SectionBlock";
 
 const ParkingViewAbonnementen = ({ parkingdata }: { parkingdata: any }) => {
-  
-  if(!parkingdata.abonnementsvorm_fietsenstalling || parkingdata.abonnementsvorm_fietsenstalling.length === 0) {
-    return null;
-  }
+
+  // if(!parkingdata.abonnementsvorm_fietsenstalling || parkingdata.abonnementsvorm_fietsenstalling.length === 0) {
+  //   return null;
+  // }
 
   return (
     <>
       <SectionBlock heading="Abonnementen">
         <div className="ml-2 grid grid-cols-3">
-          {parkingdata.abonnementsvorm_fietsenstalling.map((x) => {
+          {parkingdata.abonnementsvorm_fietsenstalling ? parkingdata.abonnementsvorm_fietsenstalling.map((x) => {
             return <Fragment key={x.naam}>
               <div className="col-span-2">{x.abonnementsvormen.naam}</div>
               <div className="text-right sm:text-center">&euro;{x.abonnementsvormen.prijs.toLocaleString('nl-NL')}</div>
             </Fragment>
-          })}
+          }) : <></>}
           <div className="text-right sm:text-center">
             <Button className="mt-4" onClick={() => {
               window.open('https://veiligstallen.nl/utrecht/abonnement', '_blank');
