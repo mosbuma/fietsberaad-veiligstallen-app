0eb1a7cb5619cd4ed9d42436ba08c45e0c94c48f
bartwr <mail@bartroorda.nl>
2025-04-16 16:12:15 +0200

    Fix "Failed to execute 'getComputedStyle' on 'Window'" error for chart

--- src/components/beheer/reports/LineChart.tsx
+++ src/components/beheer/reports/LineChart.tsx
@@ -3,7 +3,10 @@
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from "react";
 
-const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });// https://stackoverflow.com/a/68844119
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,  // This will disable server-side rendering for this component
+});
+// https://stackoverflow.com/a/68844119
 // const ApexCharts: any = dynamic(() => import('react-apexcharts'), { ssr: false });// https://stackoverflow.com/a/70010953
 // https://github.com/apexcharts/react-apexcharts/issues/526#issuecomment-1735973058
 
@@ -32,19 +35,27 @@ const LineChart = ({
       series: series
     });
   }, [
-    // options,
-    // series
+    options,
+    series
   ]);
+
+  if (!chartData.options || !chartData.series) {
+    return <>Loading..</>
+  }
 
   return (
     <>
-      {chartData.options && <Chart
-        options={chartData.options}
-        series={chartData.series}
-        type={type}
-        height="100%"
-        style={{ minHeight: "400px", ...style }}
-      />}
+      <div style={{ width: '100%', height: '100%' }}>
+        {typeof window !== 'undefined' && chartData.options && (
+          <Chart
+            options={chartData.options}
+            series={chartData.series}
+            type={type}
+            height="100%"
+            style={{ minHeight: "400px", ...style }}
+          />
+        )}
+      </div>
     </>
   )
 }
