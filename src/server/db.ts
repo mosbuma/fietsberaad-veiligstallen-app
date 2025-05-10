ef5f5383f04080acbe14d5b64f10ce7fd216dd88
Marc Buma <mosbuma@bumos.nl>
2025-05-10 12:26:52 +0200

    local build using dockerfile is working now

--- src/server/db.ts
+++ src/server/db.ts
@@ -7,8 +7,8 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["error", "warn"] : ["error"], // "query" -> add to development to see queries
+    // log: env.NODE_ENV === "development" ? ["error", "warn"] : ["error"], // "query" -> add to development to see queries
+    log: ["error"],
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
