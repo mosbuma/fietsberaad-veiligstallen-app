d73402013c0c075571b4015eeb416d9d4e5203ce
Marc Buma <mosbuma@bumos.nl>
2025-06-06 17:27:08 +0200

    some changes to scripts

--- azure/az-app-tail-docker-logs.sh
+++ azure/az-app-tail-docker-logs.sh
@@ -1,7 +1,7 @@
 set -o allexport; source .env; set +o allexport
 
 # az webapp log tail --name $AZURE_WEBAPP_NAME --resource-group $AZURE_RESOURCE_GROUP 
-az webapp log tail -n 1000 \
+az webapp log tail -n 100000 \
     --name $AZURE_WEBAPP_NAME \
     --resource-group $AZURE_RESOURCE_GROUP
 
