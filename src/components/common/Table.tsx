bc2ddc1eb5355658c633ef7ccc2d830014fb6dbc
bartwr <mail@bartroorda.nl>
2025-06-10 15:00:45 +0200

    Create generic Table component

---
+++ src/components/common/Table.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export interface Column<T> {
+  header: string;
+  accessor: keyof T | ((item: T) => React.ReactNode);
+  className?: string;
+}
+
+interface TableProps<T> {
+  columns: Column<T>[];
+  data: T[];
+  className?: string;
+  onRowClick?: (item: T) => void;
+}
+
+export function Table<T>({ columns, data, className = '', onRowClick }: TableProps<T>) {
+  return (
+    <div className="overflow-x-auto">
+      <table className={`min-w-full bg-white ${className}`}>
+        <thead>
+          <tr>
+            {columns.map((column, index) => (
+              <th 
+                key={index} 
+                className={`py-2 px-4 text-left ${column.className || ''}`}
+              >
+                {column.header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, rowIndex) => (
+            <tr 
+              key={rowIndex}
+              onClick={() => onRowClick?.(item)}
+              className={onRowClick ? 'cursor-pointer hover:bg-gray-50' : ''}
+            >
+              {columns.map((column, colIndex) => (
+                <td 
+                  key={colIndex} 
+                  className={`border px-4 py-2 ${column.className || ''}`}
+                >
+                  {typeof column.accessor === 'function'
+                    ? column.accessor(item)
+                    : String(item[column.accessor])}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+} 
